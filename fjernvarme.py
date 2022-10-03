32a97cfbdaa2e79dea52d0bee88eb9c83837cca2
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2022-10-03 10:09:41 +0200

    Updated script

--- fjernvarme.py
+++ fjernvarme.py
@@ -1,9 +1,8 @@
 import requests
 import hashlib
 import plotly.express as px
-import numpy as np
+import pandas as pd
 from datetime import date, datetime, timedelta
-import os
 
 FJERN_USERID = os.environ['FJERN_USERID']
 FJERN_PW = os.environ['FJERN_PW']
@@ -61,10 +60,10 @@ class fjernvarmeData:
                     date = datetime.strptime(x[1], "%d-%m-%Y").date()
                     desc = x[2]
                     total = int(x[3])
-                    consump = int(x[4]) if x[4] else 0
-                    hours = int(x[10])
-                    hours_elapsed = int(x[11]) if x[11] else 0
-                    self.data.append([date, desc, total, consump, hours, hours_elapsed])
+                    usage = int(x[4]) if x[4] else 0
+                    cum_hours = int(x[10])
+                    hours = int(x[11]) if x[11] else 0
+                    self.data.append([date, desc, total, usage, cum_hours, hours])
             print("Data succesfully parsed.")
         else:
             print("No raw data available. Nothing was parsed.")
@@ -72,7 +71,7 @@ class fjernvarmeData:
     def save_data(self, filename=f"fjernvarmedata_{date.today()}"):
         if self.data:
             with open(f"{filename}.csv", "w") as f:
-                f.write("date;type;total;consump;hours;hours_elapsed\n")
+                f.write("date;type;total;usage;cum_hours;hours\n")
                 for row in self.data:
                     for i, item in enumerate(row):
                         if i == 0:
@@ -88,51 +87,48 @@ class fjernvarmeData:
     def load_data(self, filename):
         with open(filename) as f:
             self.data = []
-            for i, line in enumerate(f.read().split("\n")): 
+            for i, line in enumerate(f.read().split("\n")):
                 if i == 0 or not line:
                     continue
-                date, desc, total, consump, hours, hours_elapsed, _ = line.split(";")
+                date, desc, total, usage, cum_hours, hours, _ = line.split(";")
                 date = datetime.strptime(date, "%d-%m-%Y").date()
                 total = int(total)
-                consump = int(consump)
+                usage = int(usage)
+                cum_hours = int(cum_hours)
                 hours = int(hours)
-                hours_elapsed = int(hours_elapsed)
-                self.data.append([date, desc, total, consump, hours, hours_elapsed])
+                self.data.append([date, desc, total, usage, cum_hours, hours])
 
 
 fjernvarme = fjernvarmeData(user_id=FJERN_USERID, password=FJERN_PW)
 fjernvarme.get_raw_data()
 fjernvarme.parse_raw_data()
-
-daily_data = np.array(fjernvarme.data)
-daily_data = daily_data[daily_data[:,1] == 'Mellemafl.']
-years = set(map(lambda x: x.year, daily_data[:,0]))
-months = set(map(lambda x: x.month, daily_data[:,0]))
+daily_data = pd.DataFrame(fjernvarme.data, columns=['date', 'desc', 'total', 'usage', 'cum_hours', 'hours'])
+daily_data = daily_data[daily_data['desc'] == 'Mellemafl.']
+years = set(map(lambda x: x.year, daily_data['date']))
+months = set(map(lambda x: x.month, daily_data['date']))
 monthly_data = []
+
 for year in years:
     for month in months:
-        s = sum(daily_data[[(x.month == month) & (x.year == year) for x in daily_data[:,0]], 3])
+        w = [(x.month == month) & (x.year == year) for x in daily_data['date']]
+        s = sum(daily_data[w]['usage'])
         if s:
             monthly_data.append((date(year, month, 1), s))
 
-monthly_data = np.array(monthly_data)
+monthly_data = pd.DataFrame(monthly_data, columns=['date', 'usage'])
 
-def make_daily_plot(data, range_dates = None, range_hours = (22, 26)):
-    if range_hours:
-        data = data[(range_hours[0] <= data[:, 5]) & (data[:, 5] <= range_hours[1]),:]
+
+def make_plot(data, range_dates=None, daily=True):
     if range_dates:
-        data = data[(range_dates[0] <= data[:, 0]) & (data[:, 0] <= range_dates[1]),:]
-    if data.size > 0:
-        fig = px.scatter(x = data[:,0], y = data[:,3], labels={"x": "Dato", "y": "Forbrug (kWh)"})
-        return fig
-    else:
+        data = data[(range_dates[0] <= data['date']) & (data['date'] <= range_dates[1])]
+    if data.size == 0:
         return None
-
-def make_monthly_plot(data, range_dates = None):
-    if range_dates:
-        data = data[(range_dates[0] <= data[:, 0]) & (data[:, 0] <= range_dates[1]),:]
-    fig = px.scatter(x = data[:,0], y = data[:,1], labels={"x": "Dato", "y": "Forbrug (kWh)"})
+    if daily:
+        fig = px.scatter(data, x='date', y='usage', color='hours', labels={"date": "Dato", "usage": "Forbrug (kWh)"}, color_continuous_scale='agsunset')
+    else:
+        fig = px.scatter(data, x='date', y='usage', labels={"x": "Dato", "y": "Forbrug (kWh)"})
     return fig
+
 
 today = datetime.today()
 last_day_prev_month = today.replace(day=1) - timedelta(days=1)
@@ -141,37 +137,31 @@ prev_month = (date(last_day_prev_month.year, last_day_prev_month.month, 1), last
 current_year = (date(today.year, 1, 1), today.date())
 figs = []
 
-fig1 = make_daily_plot(daily_data, range_dates = current_month)
+fig1 = make_plot(daily_data, range_dates=current_month)
 if fig1:
-    fig1.update_layout(title_text="Forbrug per dag i indeværende måned", title_x=0.5)
+    fig1.update_layout(title_text="Forbrug per dag i indeværende måned", title_x=0.5, coloraxis_colorbar=dict(title="Antal timer", tickvals=[0, 10, 20, 30, 40, 50]))
     figs.append(fig1)
-# fig1.write_html("daily_usage_this_month.html")
 
-fig2 = make_daily_plot(daily_data, range_dates = prev_month)
-fig2.update_layout(title_text="Forbrug per dag i forrige måned", title_x=0.5)
+fig2 = make_plot(daily_data, range_dates=prev_month)
+fig2.update_layout(title_text="Forbrug per dag i forrige måned", title_x=0.5, coloraxis_colorbar=dict(title="Antal timer", tickvals=[0, 10, 20, 30, 40, 50]))
 figs.append(fig2)
-# fig2.write_html("daily_usage_prev_month.html")
 
-fig3 = make_daily_plot(daily_data, range_dates = current_year)
-fig3.update_layout(title_text="Forbrug per dag i indeværende år", title_x=0.5)
+fig3 = make_plot(daily_data, range_dates=current_year)
+fig3.update_layout(title_text="Forbrug per dag i indeværende år", title_x=0.5, coloraxis_colorbar=dict(title="Antal timer", tickvals=[0, 10, 20, 30, 40, 50]))
 figs.append(fig3)
-# fig3.write_html("daily_usage_this_year.html")
 
-fig4 = make_daily_plot(daily_data)
-fig4.update_layout(title_text="Forbrug per dag altid", title_x=0.5)
-figs.append(fig4)
-# fig4.write_html("daily_usage_all_time.html")
+fig4 = make_plot(daily_data)
+fig4.update_layout(title_text="Forbrug per dag altid", title_x=0.5, coloraxis_colorbar=dict(title="Antal timer", tickvals=[0, 10, 20, 30, 40, 50]))
+figs.append(fig3)
 
 current_year = (date(today.year, 1, 1), date(today.year, today.month, 1))
-fig5 = make_monthly_plot(monthly_data, current_year)
+fig5 = make_plot(monthly_data, range_dates=current_year, daily=False)
 fig5.update_layout(title_text="Forbrug per måned i indeværende år", title_x=0.5)
 figs.append(fig5)
-# fig5.write_html("monthly_usage_this_year.html")
 
-fig6 = make_monthly_plot(monthly_data)
+fig6 = make_plot(monthly_data, daily=False)
 fig6.update_layout(title_text="Forbrug per måned altid", title_x=0.5)
 figs.append(fig6)
-# fig6.write_html("monthly_usage_all_time.html")
 
 with open('index.html', 'w') as f:
     for fig in figs:
